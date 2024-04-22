678a979b20961ca7abe387de06e661062f9c7463
Code-KVD <kvd.sridhar.code@gmail.com>
2024-04-22 18:52:15 +0530

    removed critical  issue

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -95,15 +95,6 @@ const registerUser = asyncHandler( async (req,res)=>{
     return res.status(201).json(
         new ApiResponse(200, createdUser, "User successfully created!")
     )
-
-
-
-    
-
-
-    
-
-
 });
 
 
